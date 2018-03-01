433ace016a08d74b773cf84ec7b9b3cb6088c868
sandorstormen <sandor.berglund@gmail.com>
2018-03-01 13:20:48 +0100

    Commit

--- memcpy.h
+++ memcpy.h
@@ -1,6 +1,7 @@
-#ifndef SPRITES
-#define SPRITES
-void memcpy ( void * destination, const void * source, int num ) {
+#ifndef MEMCPY
+#define MEMCPY
+// Made my own memcpy function beacuse the chipkit doesn't have one
+void memcpy ( int * destination, const int * source, int num ) {
 	int i;
 	for(i=0; i < num; i++) {
 		destination[0] = source[0];
